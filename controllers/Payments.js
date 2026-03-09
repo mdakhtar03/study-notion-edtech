0a6a8fb1686acbdf90c860807f87c4b912c53f63
unknown <mdakhtarlog@gmail.com>
2026-03-09 19:43:07 +0530

    Payment with RazorPay added

--- controllers/Payments.js
+++ controllers/Payments.js
@@ -89,3 +89,71 @@ exports.capturePayment = async (req,res)=>{
 };
 
 //verify signature of Razorpay and server
+exports.verifySignature = async (req, res)=>{
+    try {
+        // match server secret and payment secret 
+        const webhookSecret = "12345678"
+
+        const signature = req.headers["x-razorpay-signature"];
+
+        const shasum = crypto.createHmac("sha256",webhookSecret)
+        shasum.update(JSON.stringify(req.body))
+        const digest = shasum.digest("hex")
+
+        if(signature === digest){
+            console.log("Payment is authorized")
+            
+            //entroll student to the course
+            const {courseId,userId} = req.body.payload.payment.entity.notes;
+            try {
+                const enrolledCourse = await Course.findByIdAndUpdate(courseId,{
+                    $push: {studentEnrolled:userId}
+                },{new:true})
+
+                if(!enrolledCourse){
+                    return res.status(500).json({
+                        success:false,
+                        message:"Course not found"
+                    })
+                }
+                console.log(enrolledCourse)
+                //find the student and add course to their list enrolled
+                const enrolledStudent = await User.findByIdAndUpdate(userId,
+                    {$push:{courses:courseId}},{new:true}
+                )
+                console.log(enrolled)
+
+                //send mail for confirmation
+                const emailResponse = await mailSender(
+                    enrolledStudent.email,
+                    "Congratulations, from StudyNotion",
+                    "Congratulations, you are onboarded into new StudyNotion"
+                )
+                console.log(emailResponse)
+                return res.status(200).json({
+                    success:true,
+                    message:"signature verified and course Added"
+                })
+            } catch (error) {
+                console.log(error);
+                return res.status(500).json({
+                    success:false,
+                    message:error.message
+                })
+            }
+        }
+
+        else{
+            return res.status(400).json({
+                success:false,
+                message:"Invalid request"
+            })
+        }
+    } catch (error) {
+        console.log(error);
+                return res.status(500).json({
+                    success:false,
+                    message:error.message
+                })
+    }
+}
